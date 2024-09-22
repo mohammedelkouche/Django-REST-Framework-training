d9006f20a80515a2c1ba9948dd98af309c356429
Mohammed El Kouche <mohammed0elkouche@gmail.com>
2024-09-22 02:57:31 +0100

    add urls.py and request.method == 'POST'

--- rest_api/views.py
+++ rest_api/views.py
@@ -7,10 +7,19 @@ from rest_framework.parsers import JSONParser
 
 # Create your views here.
 
-def Posts(request):
+def PostsView(request):
 
     if request.method == 'GET':
         posts = Post.objects.all() #querySet
-        #if you have fecth all the data fome the table querySet use many=True
-        serialaze = PostSerializer(Post, many=True)
-        return JsonResponse(serialaze.data, safe =False)
+        #if you have fecth all the data fome the table (querySet) use many=True
+        serialazer = PostSerializer(posts, many=True)
+        return JsonResponse(serialazer.data, safe =False)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serialazer = PostSerializer(data = data)
+        
+        if serialazer.is_valid():
+            serialazer.save()
+            return JsonResponse(serialazer.data, status=201)
+        return JsonResponse(serialazer.errors, status=400)
