2c4c5c3fc22460f0c8df335ce9f5a8e49d056f87
Mohammed El Kouche <mohammed0elkouche@gmail.com>
2024-09-23 13:30:04 +0100

    @api_view decorator for working with function based views

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,50 +1,48 @@
 from django.shortcuts import render
 from .models import Post
 from .serializers import PostSerializer
-from django.http import JsonResponse, HttpResponse
-from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def PostsView(request):
 
     if request.method == 'GET':
         posts = Post.objects.all() #querySet
         #if you have fecth all the data fome the table (querySet) use many=True
         serialazer = PostSerializer(posts, many=True)
-        return JsonResponse(serialazer.data, safe =False)
+        return Response(serialazer.data)
     
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serialazer = PostSerializer(data = data)
+        serialazer = PostSerializer(data = request.data)
         
         if serialazer.is_valid():
             serialazer.save()
-            return JsonResponse(serialazer.data, status=201)
-        return JsonResponse(serialazer.errors, status=400)
+            return Response(serialazer.data, status=status.HTTP_201_CREATED)
+        return Response(serialazer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
 #pk : primary kay 
 # we can pass id instead of pk
-@csrf_exempt
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def posts_detail(request, pk):
     try:
         post = Post.objects.get(pk=pk)  # instanse
     except post.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         serializer = PostSerializer(post)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     elif request.method == 'PUT': #update
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(post, data=data)
+        serializer = PostSerializer(post, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         post.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
