d9dd5c8d189b45ecfbea04f4669cec6cfabb14a0
Mohammed El Kouche <mohammed0elkouche@gmail.com>
2024-09-23 01:15:26 +0100

    Writing regular Django views using our Serializer

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Post
 from .serializers import PostSerializer
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from rest_framework.parsers import JSONParser
-
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def PostsView(request):
 
     if request.method == 'GET':
@@ -23,3 +24,27 @@ def PostsView(request):
             serialazer.save()
             return JsonResponse(serialazer.data, status=201)
         return JsonResponse(serialazer.errors, status=400)
+    
+
+#pk : primary kay 
+# we can pass id instead of pk
+@csrf_exempt
+def posts_detail(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)  # instanse
+    except post.DoesNotExist:
+        return HttpResponse(status=404)
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT': #update
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(post, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        post.delete()
+        return HttpResponse(status=204)
